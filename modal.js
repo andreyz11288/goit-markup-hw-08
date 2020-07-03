e561d78a4026338746f2e78cd85e3316423f6e37
Andrey Zatochniy <andreyz11288@gmail.com>
2020-07-03 22:19:28 +0300

    3

--- modal.js
+++ modal.js
@@ -2,19 +2,20 @@
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
+    closeModalBtnGo: document.querySelector("[data-modal-close-go]"),
     modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtnGo.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
 })();
 (() => {
-  document.querySelector(".js-speaker-form")
-  .addEventListener("submit", (e) => {
+  document.querySelector(".js-speaker-form").addEventListener("submit", (e) => {
     e.preventDefault();
 
     new FormData(e.currentTarget).forEach((value, name) =>
